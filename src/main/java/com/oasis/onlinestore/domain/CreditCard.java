a9e5097ca19ffcb540d45550675a2b27a7b218ae
Chrisfel Afanam <chrisfela17@gmail.com>
2023-05-21 21:00:09 -0500

    CreditCard entity added

---
+++ src/main/java/com/oasis/onlinestore/domain/CreditCard.java
@@ -0,0 +1,26 @@
+package com.oasis.onlinestore.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Entity
+@Data
+public class CreditCard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String cardNumber;
+    private int expirationDate;
+    private String nameOnCard;
+    private int securityCode;
+
+    public CreditCard(){
+
+    }
+}
