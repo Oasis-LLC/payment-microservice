17104e432f71ef2433f18524685e9d58f386be8e
Chrisfel Afanam <chrisfela17@gmail.com>
2023-05-21 20:50:13 -0500

    LineItem and Review entities added

---
+++ src/main/java/com/oasis/onlinestore/domain/LineItem.java
@@ -0,0 +1,24 @@
+package com.oasis.onlinestore.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Entity
+@Data
+public class LineItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private int quantity;
+    private double discount;
+    private Object item;
+
+    public LineItem(){
+
+    }
+
+
+
+}
---
+++ src/main/java/com/oasis/onlinestore/domain/Review.java
@@ -0,0 +1,32 @@
+package com.oasis.onlinestore.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Data
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private int numberOfStars;
+    private String title;
+    private String date;
+    private String description;
+
+    private Customer buyer;
+
+
+
+    public Review() {
+
+    }
+
+
+}
